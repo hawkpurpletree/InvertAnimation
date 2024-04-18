acf9c505b2c53de43eb0b5898265f0cc70d2602f
hawk <nahuel36gg@gmail.com>
2024-04-18 13:47:08 -0300

    comit inicial, funciona la inversion pero estira todas las extremidades

---
+++ Assets/Editor/AnimationMirrorWindow.cs
@@ -0,0 +1,327 @@
+using PlasticGui.Configuration.CloudEdition.Welcome;
+using PlasticPipe.PlasticProtocol.Messages;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.UIElements;
+//using UnityEngine.Animations;
+//using UnityEngine.Playables;
+
+public class AnimationMirrorWindow : EditorWindow
+{
+    ObjectField tpose;
+    ObjectField originAnimation;
+    ObjectField destinyAnimation;
+
+
+    [MenuItem("PurpleTree/Animation Editor")]
+    public static void ShowExample()
+    {
+        AnimationMirrorWindow wnd = GetWindow<AnimationMirrorWindow>();
+        wnd.titleContent = new GUIContent("Animation Editor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        Label label = new Label("");
+        root.Add(label);
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy
+        Label label2 = new Label("Invertir Animacion");
+        StyleEnum<TextAnchor> align = new StyleEnum<TextAnchor>();
+        align.value = TextAnchor.MiddleCenter;
+        label2.style.unityTextAlign = align;
+        root.Add(label2);
+
+        Label label3 = new Label("");
+        root.Add(label3);
+
+        originAnimation = new ObjectField();
+        originAnimation.objectType = typeof(AnimationClip);
+        originAnimation.name = "animacionOrigen";
+        originAnimation.label = "Animación origen";
+        root.Add(originAnimation);
+
+        destinyAnimation = new ObjectField();
+        destinyAnimation.objectType = typeof(AnimationClip);
+        destinyAnimation.name = "animacionDestino";
+        destinyAnimation.label = "Animación destino";
+        root.Add(destinyAnimation);
+
+        tpose = new ObjectField();
+        tpose.objectType = typeof(GameObject);
+        tpose.RegisterValueChangedCallback((obj) => {
+            if (PrefabUtility.GetPrefabAssetType(obj.newValue) == PrefabAssetType.Regular)
+            {
+                if (PrefabUtility.IsPartOfPrefabInstance(obj.newValue))
+                    tpose.value = PrefabUtility.GetOutermostPrefabInstanceRoot(obj.newValue);
+                else
+                    tpose.value = obj.newValue;
+            }
+            else
+                tpose.value = null;
+        });
+        tpose.name = "tpose";
+        tpose.label = "Prefab T-Pose";
+        root.Add(tpose);
+
+        // Create button
+        Button button = new Button();
+        button.name = "invertir";
+        button.text = "Invertir";
+        button.clicked += InvertAnimation;
+        root.Add(button);
+
+    }
+
+
+    private void AddAllKeysToArray(int quaternionpos, int keyIndex, ref Dictionary<string, Dictionary<int, float[]>> rotationskeys, EditorCurveBinding[] curvesinfo, AnimationCurve animCurve)
+    {
+        float[] array = new float[animCurve.keys.Length];
+        for (int j = 0; j < animCurve.keys.Length; j++)
+            array[j] = animCurve.keys[j].value;
+        rotationskeys[curvesinfo[keyIndex].path].Add(quaternionpos, array);
+    }
+    private Dictionary<string, Dictionary<int, float[]>> CollectAllRotations(out Dictionary<string, bool> rotationSetted) 
+    {
+        EditorCurveBinding[] curvesinfo = AnimationUtility.GetCurveBindings((AnimationClip)originAnimation.value);
+        Dictionary<string, Dictionary<int, float[]>> rotationskeys = new Dictionary<string, Dictionary<int, float[] >>();
+        rotationSetted = new Dictionary<string, bool>();
+        for (int i = 0; i < curvesinfo.Length; i++)
+        {
+            if (!rotationskeys.ContainsKey(curvesinfo[i].path))
+            { 
+                rotationskeys.Add(curvesinfo[i].path, new Dictionary<int, float[] >());
+                rotationSetted.Add(curvesinfo[i].path, false);
+            }
+            if (curvesinfo[i].propertyName == "m_LocalRotation.x" || curvesinfo[i].propertyName == "m_LocalRotation.y" || curvesinfo[i].propertyName == "m_LocalRotation.z" || curvesinfo[i].propertyName == "m_LocalRotation.w")
+            {
+                AnimationCurve animCurve = AnimationUtility.GetEditorCurve((AnimationClip)originAnimation.value, curvesinfo[i]);
+                if (curvesinfo[i].propertyName == "m_LocalRotation.x")
+                {
+                    AddAllKeysToArray(0, i,ref rotationskeys, curvesinfo, animCurve);
+                }
+                else if (curvesinfo[i].propertyName == "m_LocalRotation.y")
+                {
+                    AddAllKeysToArray(1, i, ref rotationskeys, curvesinfo, animCurve);
+
+                }
+                if (curvesinfo[i].propertyName == "m_LocalRotation.z")
+                {
+                    AddAllKeysToArray(2, i, ref rotationskeys, curvesinfo, animCurve);
+
+                }
+                if (curvesinfo[i].propertyName == "m_LocalRotation.w")
+                {
+                    AddAllKeysToArray(3, i, ref rotationskeys, curvesinfo, animCurve);
+
+                }
+            }
+        }
+        return rotationskeys;
+    }
+
+    private Quaternion ConvertArrayToQuaternion(Dictionary<string, Dictionary<int, float[]>> rotationskeys, string path, int index) 
+    {
+        Quaternion quat = new Quaternion(rotationskeys.GetValueOrDefault(path).GetValueOrDefault(0)[index],
+        rotationskeys.GetValueOrDefault(path).GetValueOrDefault(1)[index],
+        rotationskeys.GetValueOrDefault(path).GetValueOrDefault(2)[index],
+        rotationskeys.GetValueOrDefault(path).GetValueOrDefault(3)[index]);
+
+        return quat;
+    }
+
+    private Vector3 ConvertArrayToEuler(Dictionary<string, Dictionary<int, float[]>> rotationskeys, string path, int index)
+    {
+        Vector3 vec = new Vector3(rotationskeys.GetValueOrDefault(path).GetValueOrDefault(0)[index],
+        rotationskeys.GetValueOrDefault(path).GetValueOrDefault(1)[index],
+        rotationskeys.GetValueOrDefault(path).GetValueOrDefault(2)[index]);
+
+        return vec;
+    }
+    public void SetKeysValue(string previousPath, string destinyPath, ref AnimationCurve destinyCurve, AnimationCurve originCurve,float tPoseOrigin, float tPoseDestiny, int signo, bool isQuaternion = false, Dictionary<string, Dictionary<int, float[]>> originRotation = null, Dictionary<string, Dictionary<int, float[]>> destinyRotation = null, char rotationpos = 'x') {
+        
+
+        if (!previousPath.Contains("Left") && !previousPath.Contains("Right"))
+        {
+            tPoseOrigin = 0;
+            tPoseDestiny = 0;
+        }
+
+        if (isQuaternion)
+        {
+            for (int k = 0; k < originCurve.keys.Length; k++)
+            {
+                if(rotationpos == 'x')
+                    destinyCurve.AddKey(originCurve.keys[k].time, Quaternion.Euler(ConvertArrayToEuler(destinyRotation, destinyPath, k)).x);
+                if (rotationpos == 'y')
+                    destinyCurve.AddKey(originCurve.keys[k].time, Quaternion.Euler(ConvertArrayToEuler(destinyRotation, destinyPath, k)).y);
+                if (rotationpos == 'z')
+                    destinyCurve.AddKey(originCurve.keys[k].time, Quaternion.Euler(ConvertArrayToEuler(destinyRotation, destinyPath, k)).z);
+                if (rotationpos == 'w')
+                    destinyCurve.AddKey(originCurve.keys[k].time, Quaternion.Euler(ConvertArrayToEuler(destinyRotation, destinyPath, k)).w);
+            }
+        }
+        else 
+        {
+            for (int k = 0; k < originCurve.keys.Length; k++)
+            {
+                destinyCurve.AddKey(originCurve.keys[k].time, tPoseOrigin + originCurve.keys[k].value * signo + tPoseDestiny);
+            }
+        }
+        
+    }
+
+    private void SavePositions(string previousPath,string destinyPath, AnimationCurve originCurve, Vector3 tPoseOrigin, Vector3 tPoseDestiny, Dictionary<string, Dictionary<int, float[]>> originRotation, ref Dictionary<string, Dictionary<int, float[]>> destinyRotation)
+    {
+        if (!destinyRotation.ContainsKey(destinyPath))
+        {
+            destinyRotation.Add(destinyPath, new Dictionary<int, float[]>());
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            int signo = 1;
+            float[] array = new float[originCurve.keys.Length];
+
+            for (int k = 0; k < originCurve.keys.Length; k++)
+            {
+                Quaternion quat = ConvertArrayToQuaternion(originRotation, previousPath, k);
+
+                float originAnimKey = 0;
+                float origin = 0;
+                float destiny = 0;
+                if (i == 0) 
+                {
+                    origin = 0;
+                    destiny = tPoseDestiny.x;
+                    signo = 1;
+                    originAnimKey = quat.eulerAngles.x;
+                }
+                if (i == 1)
+                {
+                    origin = tPoseOrigin.y;
+                    destiny = tPoseDestiny.y;
+                    signo = -1;
+                    originAnimKey = quat.eulerAngles.y;
+                }
+                if (i == 2)
+                {
+                    origin = tPoseOrigin.z;
+                    destiny = tPoseDestiny.z;
+                    signo = -1;
+                    originAnimKey = quat.eulerAngles.z;
+                }
+                if (!previousPath.Contains("Left") && !previousPath.Contains("Right"))
+                {
+                    origin = 0;
+                    destiny = 0;
+                }
+
+
+                array[k] = origin + originAnimKey * signo + destiny;
+            }
+            destinyRotation[destinyPath].Add(i, array);
+        }
+    }
+
+
+
+    private void InvertAnimation()
+    {
+        EditorCurveBinding[] originInfo = AnimationUtility.GetCurveBindings((AnimationClip)originAnimation.value);
+        Dictionary<string, bool> rotationSaved;
+        Dictionary<string, Dictionary<int, float[]>> rotationOrigin = CollectAllRotations(out rotationSaved);
+        Dictionary<string, Dictionary<int, float[]>> rotationDestiny = new Dictionary<string, Dictionary<int, float[]>>();
+
+        AnimationClipSettings settings = AnimationUtility.GetAnimationClipSettings(((AnimationClip)originAnimation.value));
+        AnimationUtility.SetAnimationClipSettings((AnimationClip)destinyAnimation.value, settings);
+
+        string previousPath = "";
+        string destinyPath = "";
+        for (int i = 0; i < originInfo.Length; i++)
+        {
+            if (((GameObject)tpose.value).transform.Find(originInfo[i].path) != null)
+            {
+                AnimationCurve originCurve = AnimationUtility.GetEditorCurve((AnimationClip)originAnimation.value, originInfo[i]);
+                AnimationCurve destinyCurve = new AnimationCurve();
+
+                previousPath = originInfo[i].path;
+                destinyPath = originInfo[i].path;
+                if (originInfo[i].path.Contains("Left") || originInfo[i].path.Contains("Right"))
+                {
+                    
+                    if (originInfo[i].path.Contains("Left"))
+                    {
+                        originInfo[i].path = originInfo[i].path.Replace("Left", "Right");
+                        //destinyPath = originInfo[i].path.Replace("Left", "Right");
+                    }
+                    else if (originInfo[i].path.Contains("Right"))
+                    {
+                        originInfo[i].path = originInfo[i].path.Replace("Right", "Left");
+                        //destinyPath = originInfo[i].path.Replace("Left", "Right");
+                    }
+                }
+                
+                
+                if (originInfo[i].propertyName == "m_LocalPosition.x")
+                {
+                    SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, ((GameObject)tpose.value).transform.Find(previousPath).localPosition.x, ((GameObject)tpose.value).transform.Find(originInfo[i].path).localPosition.x, -1);
+
+                }
+                else if (originInfo[i].propertyName == "m_LocalPosition.y")
+                {
+
+                    SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, 0, ((GameObject)tpose.value).transform.Find(originInfo[i].path).localPosition.y, 1);
+
+                }
+                else if (originInfo[i].propertyName == "m_LocalPosition.z")
+                {
+                    //Debug.Log(previousPath + ((GameObject)tpose.value).transform.Find(previousPath).localPosition.z + " " + ((GameObject)tpose.value).transform.Find(curvesinfo[i].path).localPosition.z);
+
+                    SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, 0, ((GameObject)tpose.value).transform.Find(originInfo[i].path).localPosition.z, 1);
+
+                }
+                else if (originInfo[i].propertyName == "m_LocalRotation.x" 
+                      || originInfo[i].propertyName == "m_LocalRotation.y" 
+                      || originInfo[i].propertyName == "m_LocalRotation.z" 
+                      || originInfo[i].propertyName == "m_LocalRotation.w")
+                {
+                    if (!rotationSaved[previousPath]) 
+                    { 
+                        rotationSaved[previousPath] = true;
+                        SavePositions(previousPath, originInfo[i].path, originCurve, ((GameObject)tpose.value).transform.Find(previousPath).localRotation.eulerAngles, ((GameObject)tpose.value).transform.Find(originInfo[i].path).localRotation.eulerAngles, rotationOrigin, ref rotationDestiny);
+                    
+                    }
+
+                    if(originInfo[i].propertyName == "m_LocalRotation.x")
+                        SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, 0, 0, -1, true, rotationOrigin, rotationDestiny,'x');
+                    else if (originInfo[i].propertyName == "m_LocalRotation.y")
+                        SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, 0, 0, -1, true, rotationOrigin, rotationDestiny, 'y');
+                    if (originInfo[i].propertyName == "m_LocalRotation.z")
+                        SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve, 0,0, -1, true, rotationOrigin, rotationDestiny, 'z');
+                    if (originInfo[i].propertyName == "m_LocalRotation.w")
+                        SetKeysValue(previousPath, originInfo[i].path, ref destinyCurve, originCurve,0,0, -1, true, rotationOrigin, rotationDestiny, 'w');
+
+                }
+                //estudiar cada hueso para rotacion
+                //estudiar hips y spine y neck
+                ((AnimationClip)destinyAnimation.value).SetCurve(originInfo[i].path, typeof(Transform), originInfo[i].propertyName, destinyCurve);
+                //AnimationUtility.SetEditorCurve((AnimationClip)destinyAnimation.value, originInfo[i], destinyCurve);
+                Debug.Log(previousPath + " " + originInfo[i].path);
+            }
+        }
+    }
+}
+        
+
+    
+
---
+++ Assets/MirrorPoseJob.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+
+public static class AnimationHumanStreamExtensions
+{
+    public static void MultMuscle(this UnityEngine.Animations.AnimationHumanStream stream, UnityEngine.Animations.MuscleHandle h, float f)
+    {
+        stream.SetMuscle(h, f * stream.GetMuscle(h));
+    }
+
+    public static void SwapMuscles(this UnityEngine.Animations.AnimationHumanStream stream, UnityEngine.Animations.MuscleHandle a, UnityEngine.Animations.MuscleHandle b)
+    {
+        var t = stream.GetMuscle(a);
+        stream.SetMuscle(a, stream.GetMuscle(b));
+        stream.SetMuscle(b, t);
+    }
+}
+
+public struct MirrorPoseJob : UnityEngine.Animations.IAnimationJob
+{
+    private static readonly float[] BodyDoFMirror = {
+        +1.0f,  // BodyDof.SpineFrontBack,
+        -1.0f,  // BodyDof.SpineLeftRight,
+        -1.0f,  // BodyDof.SpineRollLeftRight,
+        +1.0f,  // BodyDof.ChestFrontBack,
+        -1.0f,  // BodyDof.ChestLeftRight,
+        -1.0f,  // BodyDof.ChestRollLeftRight,
+        +1.0f,  // BodyDof.UpperChestFrontBack,
+        -1.0f,  // BodyDof.UpperChestLeftRight,
+        -1.0f   // BodyDof.UpperChestRollLeftRight,
+    };
+
+    private static readonly float[] HeadDoFMirror = {
+        +1.0f,  // HeadDof.NeckFrontBack,
+        -1.0f,  // HeadDof.NeckLeftRight,
+        -1.0f,  // HeadDof.NeckRollLeftRight,
+        +1.0f,  // HeadDof.HeadFrontBack,
+        -1.0f,  // HeadDof.HeadLeftRight,
+        -1.0f,  // HeadDof.HeadRollLeftRight,
+        +1.0f,  // HeadDof.LeftEyeDownUp,
+        -1.0f,  // HeadDof.LeftEyeLeftRight,
+        +1.0f,  // HeadDof.RightEyeDownUp,
+        -1.0f,  // HeadDof.RightEyeLeftRight,
+        +1.0f,  // HeadDof.JawDownUp,
+        -1.0f   // HeadDof.JawLeftRight,
+    };
+
+    public void ProcessAnimation(UnityEngine.Animations.AnimationStream stream)
+    {
+        var humanStream = stream.AsHuman();
+
+        //humanStream.bodyLocalPosition = Mirrored(humanStream.bodyLocalPosition);
+        //humanStream.bodyLocalRotation = Mirrored(humanStream.bodyLocalRotation);
+
+        //humanStream.bodyLocalPosition = Mirrored(humanStream.bodyLocalPosition);
+        //humanStream.bodyLocalRotation = Quaternion.Euler(humanStream.bodyLocalRotation.eulerAngles.x, -humanStream.bodyLocalRotation.eulerAngles.y, humanStream.bodyLocalRotation.eulerAngles.z);
+
+
+        humanStream.bodyPosition = Mirrored(humanStream.bodyPosition);
+        humanStream.bodyRotation = Mirrored(humanStream.bodyRotation);
+
+        // mirror body
+        for (int i = 0; i < (int)BodyDof.LastBodyDof; i++)
+        {
+            humanStream.MultMuscle(new UnityEngine.Animations.MuscleHandle((BodyDof)i), BodyDoFMirror[i]);
+        }
+
+        // mirror head
+        for (int i = 0; i < (int)HeadDof.LastHeadDof; i++)
+        {
+            humanStream.MultMuscle(new UnityEngine.Animations.MuscleHandle((HeadDof)i), HeadDoFMirror[i]);
+        }
+
+        // swap arms
+        for (int i = 0; i < (int)ArmDof.LastArmDof; i++)
+        {
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftArm, (ArmDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightArm, (ArmDof)i));
+        }
+
+        // swap legs
+        for (int i = 0; i < (int)LegDof.LastLegDof; i++)
+        {
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftLeg, (LegDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightLeg, (LegDof)i));
+        }
+
+        // swap fingers
+        for (int i = 0; i < (int)FingerDof.LastFingerDof; i++)
+        {
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftThumb, (FingerDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightThumb, (FingerDof)i));
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftIndex, (FingerDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightIndex, (FingerDof)i));
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftMiddle, (FingerDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightMiddle, (FingerDof)i));
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftRing, (FingerDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightRing, (FingerDof)i));
+            humanStream.SwapMuscles(
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.LeftLittle, (FingerDof)i),
+                new UnityEngine.Animations.MuscleHandle(HumanPartDof.RightLittle, (FingerDof)i));
+        }
+
+
+        // swap ik
+        Vector3[] goalLocalPositions = new Vector3[4];
+        Quaternion[] goalLocalRotations = new Quaternion[4];
+        Vector3[] goalPositions = new Vector3[4];
+        Quaternion[] goalRotations = new Quaternion[4];
+        float[] goalWeightPositons = new float[4];
+        float[] goalWeightRotations = new float[4];
+        Vector3[] hintPositions = new Vector3[4];
+        float[] hintWeightPositions = new float[4];
+        for (int i = 0; i < 4; i++)
+        {
+            goalLocalPositions[i] = humanStream.GetGoalLocalPosition(AvatarIKGoal.LeftFoot + i);
+            goalLocalRotations[i] = humanStream.GetGoalLocalRotation(AvatarIKGoal.LeftFoot + i);
+
+            goalPositions[i] = humanStream.GetGoalPosition(AvatarIKGoal.LeftFoot + i);
+            goalRotations[i] = humanStream.GetGoalRotation(AvatarIKGoal.LeftFoot + i);
+
+            goalWeightPositons[i] = humanStream.GetGoalWeightPosition(AvatarIKGoal.LeftFoot + i);
+            goalWeightRotations[i] = humanStream.GetGoalWeightRotation(AvatarIKGoal.LeftFoot + i);
+            hintPositions[i] = humanStream.GetHintPosition(AvatarIKHint.LeftKnee + i);
+            hintWeightPositions[i] = humanStream.GetHintWeightPosition(AvatarIKHint.LeftKnee + i);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            int j = (i + 1) % 2 + (i / 2) * 2;                  // make [1, 0, 3, 2]
+            //humanStream.SetGoalLocalPosition(AvatarIKGoal.LeftFoot + i, Mirrored(goalLocalPositions[j]));
+            //humanStream.SetGoalLocalRotation(AvatarIKGoal.LeftFoot + i, Mirrored(goalLocalRotations[j]));
+
+            humanStream.SetGoalPosition(AvatarIKGoal.LeftFoot + i, Mirrored(goalPositions[j]));
+            humanStream.SetGoalRotation(AvatarIKGoal.LeftFoot + i, Mirrored(goalRotations[j]));
+
+            humanStream.SetGoalWeightPosition(AvatarIKGoal.LeftFoot + i, goalWeightPositons[j]);
+            humanStream.SetGoalWeightRotation(AvatarIKGoal.LeftFoot + i, goalWeightRotations[j]);
+
+            humanStream.SetHintPosition(AvatarIKHint.LeftKnee + i, Mirrored(hintPositions[j]));
+            humanStream.SetHintWeightPosition(AvatarIKHint.LeftKnee + i, hintWeightPositions[j]);
+        }
+        
+
+    }
+
+    public static Vector3 Mirrored(Vector3 value)
+    {
+        return new Vector3(-value.x, value.y, value.z);
+    }
+
+    public static Vector3 Mirrored_test(Vector3 value)
+    {
+        return new Vector3(-value.x, value.y, -value.z);
+    }
+
+    public static Quaternion Mirrored(Quaternion value)
+    {
+        return Quaternion.Euler(value.eulerAngles.x, -value.eulerAngles.y, -value.eulerAngles.z);
+    }
+
+    public static Quaternion Mirrored_test(Quaternion value)
+    {
+        return Quaternion.Euler(value.eulerAngles.x, -value.eulerAngles.y, value.eulerAngles.z);
+    }
+
+    public void ProcessRootMotion(UnityEngine.Animations.AnimationStream stream) { }
+}
